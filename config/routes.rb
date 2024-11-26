50a9801c8f129e992845f47125eaf171bdd98c76
m-oit <177226299+m-oit@users.noreply.github.com>
2024-11-26 06:03:24 +0000

    途中保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  
+
   root to: 'homes#top'
   devise_for :users
+
+  resources :users, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :post_images, only: [:new, :create, :index, :show]
 
   get 'about' => 'homes#about',as: 'about'
