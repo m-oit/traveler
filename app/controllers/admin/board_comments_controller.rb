fd1ca69f8c1e59c0af50abb21d644bf273eb313b
EC2 Default User <177226299+m-oit@users.noreply.github.com>
2025-01-31 15:28:58 +0000

    2/1

--- app/controllers/admin/board_comments_controller.rb
+++ app/controllers/admin/board_comments_controller.rb
@@ -1,29 +1,14 @@
-Admin::BoardCommentsController
-layout 'admin'
-before_action :authenticate_admin!
+class Admin::BoardCommentsController < ApplicationController
+  layout 'admin'
+  before_action :authenticate_admin!
 
-module Admin
-  class BoardCommentsController < ApplicationController
-    before_action :authenticate_admin!
-    before_action :set_group
-
-    def destroy
-      @group = Group.find(params[:group_id])
-      @comment = @group.board_comments.find(params[:id])
-  
-      if current_user == @group.owner || current_user.admin?
-        @comment.destroy
-        redirect_to admin_group_path(@group), notice: 'コメントを削除しました。'
-      else
-        redirect_to admin_group_path(@group), alert: 'コメントを削除する権限がありません。'
-      end
-    end
+  def index
+    @board_comments = BoardComment.includes(:user, :group).all
   end
 
-    private
-
-    def set_group
-      @group = Group.find(params[:group_id])
-    end
+  def destroy
+    @board_comment = BoardComment.find(params[:id])
+    @board_comment.destroy
+    redirect_to admin_board_comments_path, notice: 'コメントが削除されました。'
   end
 end
