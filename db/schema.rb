0485d5bf125bbbae91ff5074a70751239aac2a5a
EC2 Default User <177226299+m-oit@users.noreply.github.com>
2025-02-08 08:58:29 +0000

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_08_045728) do
+ActiveRecord::Schema.define(version: 2025_02_08_085753) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2025_02_08_045728) do
     t.integer "group_post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_image_id"
   end
 
   create_table "group_post_comments", force: :cascade do |t|
